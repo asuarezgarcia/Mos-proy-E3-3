7bc110364d60bf630be89d5cc99cde1621be70b6
asuarezgarcia <a.suarezg@uniandes.edu.co>
2024-12-02 16:26:45 -0500

    Copiar nuevos códigos y cambiar cosas

---
+++ Solutions/ToDo-list.py
@@ -0,0 +1,125 @@
+"""
+GENERAL:
+- Crear funcion distancia haversiana
+- Crear función distancia con ORSS
+
+ESCENARIO 1) Solo que sea una solución factible (rangos, capacidades, y que cumpla demandas)
+    - No sé si para multi_vehicles toca generarlos, o solo decir que lo creamos y ya
+    - MODELO
+        - Cargar datos entrada y guardarlos en alguna estructura
+            - Cargar clientes
+            - Cargar depósitos
+            - Cargar vehículos
+        - Crear función matrices de costos usando distancias (son 2, una harversiana y otra con ORSS)
+        - Crear variables de decisión
+        - Crear modelo
+        - Poner restricciones
+        - Comprobar que funciona bien
+        - Crear función para visualización gráfica: ruta seguida por cada vehículo
+    - DOCUMENTACIÓN
+        - Cambios al modelo y su justificación
+        - Corta justificación de por qué es generalizable a cambios en los datos
+        - Reporte costo operacional: reporte detallado de los costos operacionales que contribuyen al valor de
+          la función objetivo, desglosando cada tipo de costo (distancia, tiempo, mantenimiento, etc)
+        - Análisis de resultados:
+            - Identificación de patrones o tendencias en las rutas optimizadas
+            - Problemas o desafíos encontrados
+            - Mejoras potenciales para optimizar la solución y sugerencias para ajustes futuros
+        - Documentación de cómo ejecutar el modelo en distintos casos de prueba
+    - ARCHIVOS SALIDA
+        - Rutas de vehículo (recordar que tiene nombre específico) con formato específico
+        - Valor de función objetivo ( recordar nombre específico) con formato específico
+
+
+ESCENARIO 2) Igual, pero optimizando el costo de la solución
+    - MODELO
+        - Cargar datos entrada y guardarlos en alguna estructura
+            - Cargar clientes
+            - Cargar depósitos
+            - Cargar vehículos
+        - Crear función matrices de costos usando distancias (son 2, una harversiana y otra con ORSS)
+        - Crear variables de decisión
+        - Crear modelo
+        - Poner restricciones
+        - Comprobar que funciona bien
+        - Crear función para visualización gráfica: ruta seguida por cada vehículo
+    - DOCUMENTACIÓN
+        - Cambios al modelo y su justificación
+        - Corta justificación de por qué es generalizable a cambios en los datos
+        - Reporte costo operacional: reporte detallado de los costos operacionales que contribuyen al valor de
+          la función objetivo, desglosando cada tipo de costo (distancia, tiempo, mantenimiento, etc)
+        - Análisis de resultados:
+            - Identificación de patrones o tendencias en las rutas optimizadas
+            - Problemas o desafíos encontrados
+            - Mejoras potenciales para optimizar la solución y sugerencias para ajustes futuros
+        - Documentación de cómo ejecutar el modelo en distintos casos de prueba
+    - ARCHIVOS SALIDA
+        - Rutas de vehículo (recordar que tiene nombre específico) con formato específico
+        - Valor de función objetivo ( recordar nombre específico) con formato específico
+
+
+ESCENARIO 3) Cada almacen tiene un máximo de oferta
+    - MODELO
+        - Cargar datos entrada y guardarlos en alguna estructura
+            - Cargar clientes
+            - Cargar depósitos
+            - Cargar vehículos
+        - Crear función matrices de costos usando distancias (son 2, una harversiana y otra con ORSS)
+        - Crear variables de decisión
+        - Crear modelo
+        - Poner restricciones
+        - Comprobar que funciona bien
+        - Crear función para visualización gráfica: ruta seguida por cada vehículo
+    - DOCUMENTACIÓN
+        - Cambios al modelo y su justificación
+        - Corta justificación de por qué es generalizable a cambios en los datos
+        - Reporte costo operacional: reporte detallado de los costos operacionales que contribuyen al valor de
+          la función objetivo, desglosando cada tipo de costo (distancia, tiempo, mantenimiento, etc)
+        - Análisis de resultados:
+            - Identificación de patrones o tendencias en las rutas optimizadas
+            - Problemas o desafíos encontrados
+            - Mejoras potenciales para optimizar la solución y sugerencias para ajustes futuros
+        - Documentación de cómo ejecutar el modelo en distintos casos de prueba
+    - ARCHIVOS SALIDA
+        - Rutas de vehículo (recordar que tiene nombre específico) con formato específico
+        - Valor de función objetivo ( recordar nombre específico) con formato específico
+    
+
+ESCENARIO 4) Hay más de un producto 
+    - MODELO
+        - Cargar datos entrada y guardarlos en alguna estructura
+            - Cargar clientes
+            - Cargar depósitos
+            - Cargar vehículos
+        - Crear función matrices de costos usando distancias (son 2, una harversiana y otra con ORSS)
+        - Crear variables de decisión
+        - Crear modelo
+        - Poner restricciones
+        - Comprobar que funciona bien para un producto
+        - Iterar sobre los productos
+        - Crear función para visualización gráfica: ruta seguida por cada vehículo
+    - DOCUMENTACIÓN
+        - Cambios al modelo y su justificación
+        - Corta justificación de por qué es generalizable a cambios en los datos
+        - Reporte costo operacional: reporte detallado de los costos operacionales que contribuyen al valor de
+          la función objetivo, desglosando cada tipo de costo (distancia, tiempo, mantenimiento, etc)
+        - Análisis de resultados:
+            - Identificación de patrones o tendencias en las rutas optimizadas
+            - Problemas o desafíos encontrados
+            - Mejoras potenciales para optimizar la solución y sugerencias para ajustes futuros
+        - Documentación de cómo ejecutar el modelo en distintos casos de prueba
+    - ARCHIVOS SALIDA
+        - Rutas de vehículo (recordar que tiene nombre específico) con formato específico
+        - Valor de función objetivo ( recordar nombre específico) con formato específico
+
+
+
+
+
+
+
+
+
+
+
+"""
